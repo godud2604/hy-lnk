950b10fbde271850280292ae3c160333839072be
godud2604 <ees238@naver.com>
2025-04-08 14:48:27 +0900

    feat: add initial Next.js configuration with ESLint settings

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true,  // 빌드할 때 ESLint 에러 무시
+    },
+  }
+  
+  module.exports = nextConfig;
+  
